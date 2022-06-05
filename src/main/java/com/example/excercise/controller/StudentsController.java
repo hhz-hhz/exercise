01361ddbfd9b8c00bfd964f0076f49041a5aabca
Hezi HE <hezi.he@thoughtworks.com>
2022-06-05 19:35:16 +0800

    add getmapping in controller

--- src/main/java/com/example/excercise/controller/StudentsController.java
+++ src/main/java/com/example/excercise/controller/StudentsController.java
@@ -8,6 +8,9 @@ import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,10 @@ public class StudentsController {
     return StudentIdResponse.builder()
         .id(student.getId())
         .build();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<String> getStudent(@PathVariable Integer id){
+    return ResponseEntity.ok(id.toString());
   }
 }
