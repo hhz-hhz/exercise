f34972d1d9ff4f10772e14246946d80c31cb2bfd
Hezi HE <hezi.he@thoughtworks.com>
2022-06-05 19:30:41 +0800

    add /students

--- src/main/java/com/example/excercise/controller/StudentsController.java
+++ src/main/java/com/example/excercise/controller/StudentsController.java
@@ -10,10 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
+@RequestMapping("/students")
 public class StudentsController {
 
   private StudentsService studentsService;
